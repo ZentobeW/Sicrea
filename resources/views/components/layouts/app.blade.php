154168441a6b7429bb3a1c4b64d9f1d5c83e16d5
ZentobeW <sasageyoshin02@gmail.com>
2025-12-07 12:46:58 +0700

    add Alpine JS

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @stack('styles')
     <style>
         :root{
